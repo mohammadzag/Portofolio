7af5a10cd90a70eebd63bd1e2e8db60a0e2c16d7
mohammadzag <mohammad.husain1878@gmail.com>
2026-08-08 00:14:40 +0300

    fix: enforce cache busting on CV PDF links

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     break;
                 case 'cv':
                     printLine('Payload: mohammad_cv.pdf');
-                    printLine('<a href="assets/mohammad_cv.pdf" download class="text-green"><i class="fa-solid fa-file-arrow-down"></i> [CLICK TO DOWNLOAD RESUME]</a>');
+                    printLine('<a href="assets/mohammad_cv.pdf?v=2.0" download class="text-green"><i class="fa-solid fa-file-arrow-down"></i> [CLICK TO DOWNLOAD RESUME]</a>');
                     break;
                 case 'clear':
                     terminalOutput.innerHTML = '';
